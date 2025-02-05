7c3aff144f1a36a4a960f68a497ae839babdfab6
Markus <markusmhenche@gmail.com>
2025-02-05 15:16:30 +0100

    Character commit 4 - Funcinamiento cartas
    Con ayuda del profe funciona la selección de personajes, falta hacer unos toques para que se vea mejor.

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+
+    //Section HUD Maps-------------------------------------------
     let index = 0; 
     const totalItems = $('.caja1').length; 
     const itemWidth = $('.caja1').outerWidth(true); 
@@ -26,5 +28,18 @@ $(document).ready(function () {
 
     $('.flecha.izquierda').click(function () {
         moverCarrusel(-1); // Mover a la izquierda
+    });
+
+    //Section HUD Characters--------------------------------------------
+
+    let accBtn = $(".character-box");
+
+    accBtn.on("click", function(e){
+        e.preventDefault();
+        console.log($(this).data('char'));
+        let val_char = $(this).data('char');
+
+        $('.card-box').hide();        
+        $('.card-box[data-char="' + val_char + '"]').fadeIn();
     });
 });
